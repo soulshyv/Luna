8a9f6bfffdaac717761fdf573128c88fcd5fccd8
soulshyv <fra62@sfr.fr>
2022-08-07 19:14:03 +0200

    Champ required à la création du compte

--- Luna.Web/ViewModels/RegistrationViewModel.cs
+++ Luna.Web/ViewModels/RegistrationViewModel.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Luna.ViewModels
 {
     public class RegistrationViewModel
     {
+        [Required]
         public string FirstName { get; set; }
+        
+        [Required]
         public string LastName { get; set; }
+        
+        [Required]
         public string Email { get; set; }
     }
 }
